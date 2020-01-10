7a1913687801700e38c621c8f0d65ce4adcecc8d
dharam.kc@georgebrown.ca <dharam.kc@georgebrown.ca>
2020-01-10 12:55:36 -0500

    Flight,Bookings and Customer Management - CRUD using C#

---
+++ flight_booking_c_sharp/Home.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookFlight
+{
+    public partial class Home : Form
+    {
+        public AirlineCoordinator bookFlightData;
+        public Home(AirlineCoordinator allData)
+        {
+            bookFlightData = allData;
+            InitializeComponent();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddCustomer_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Add_Customer form = new Add_Customer(bookFlightData);
+            form.Show();
+        }
+
+        private void btnListCustomer_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            List_Customer form = new List_Customer(bookFlightData);
+            form.Show();
+        }
+    }
+}
